5d7b69354e92b992471cb20dc8983c388e91d099
özge baykara sarı <baykaraozge@gmail.com>
2020-10-25 21:30:23 +0300

    Add project files.

---
+++ ASPNetCoreApplication/Pages/Contact.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Infrastructure;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASPNetCoreApplication.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Your contact page.";
+            string x= ApiCall.GetValueById(555);
+            Message += x;
+        }
+    }
+}
